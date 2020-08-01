9e8b8fc5ed03fb9c594c22ccce9a29ef0f283115
rizwan <iamrizwan40@gmail.com>
2020-08-01 00:07:26 -0700

    added a bottom scroll button

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // document.querySelectorAll('#overall').forEach(overall => {
-    //     overall.style.display='block';
-    // })
+    document.querySelector('input.search').addEventListener('focus', function(){
+        document.querySelector('div.search-group').style.boxShadow = "0px 0px 3px rgba(2, 195, 154,.5)"
+    })
+    document.querySelector('input.search').addEventListener('blur', function(){
+        document.querySelector('div.search-group').style.boxShadow = "none"
+        document.querySelector('div.search-group').style.transition = "200ms ease-in-out"
+    })
 
-    document.querySelectorAll('#country').forEach(function(country){
+    window.onscroll = function(){
+        const doc = document.documentElement.scrollHeight;
+        const winHeight = window.innerHeight;        
+        if(window.scrollY > winHeight+doc/2-winHeight){
+            document.querySelector('.go-to-bottom').classList.add('toggle-go-to-bottom');            
+        }else{
+            document.querySelector('.go-to-bottom').classList.remove('toggle-go-to-bottom');            
+        }        
+
+    }
+
+    const countries = document.querySelectorAll('#country')
+    countries[countries.length-1].setAttribute('id', "last-country");
+    countries.forEach(function(country, index){               
         function toggelData(id){
             country.querySelectorAll('.info').forEach(info => {
                 info.style.display = "none";
@@ -24,5 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 toggelData(this.dataset.covid19)
             }
         })    
-    })    
+    }) 
+      
 })
