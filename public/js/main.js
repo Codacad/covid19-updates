a29902c1bd10e6ec099dfd73bf478e048a5814c8
rizwan <iamrizwan40@gmail.com>
2020-08-01 08:19:27 -0700

    added search feature

--- public/js/main.js
+++ public/js/main.js
@@ -7,19 +7,40 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('div.search-group').style.transition = "200ms ease-in-out"
     })
 
-    window.onscroll = function(){
-        const doc = document.documentElement.scrollHeight;
-        const winHeight = window.innerHeight;        
-        if(window.scrollY > winHeight+doc/2-winHeight){
-            document.querySelector('.go-to-bottom').classList.add('toggle-go-to-bottom');            
-        }else{
-            document.querySelector('.go-to-bottom').classList.remove('toggle-go-to-bottom');            
-        }        
-
-    }
-
+    document.querySelector('input.search').addEventListener('keyup', function(e){
+        const countries = document.querySelectorAll('.country');        
+        countries.forEach(country => {
+            const header = country.querySelector('.header');
+            const countryIdentity = header.querySelector('.country-identity')
+            const countryName = countryIdentity.querySelector('h2.country-name')
+            const countryLowerCase = countryName.innerHTML.toLowerCase()
+            const serachLowercase = e.target.value.toLowerCase().trim()
+            if(countryLowerCase.indexOf(serachLowercase) == -1){
+                country.style.display = "none"
+            }else{
+                country.style.display = "block"
+            }
+        })
+    })
     const countries = document.querySelectorAll('#country')
     countries[countries.length-1].setAttribute('id', "last-country");
+    countries[0].setAttribute('id', "first-country");
+    window.addEventListener('load', function(){
+        const firstCountry = document.querySelector('#first-country');
+        firstCountry.querySelector('div.pop-popup').classList.add('show-popup');
+    })
+    window.onscroll = function(){
+        const doc = document.documentElement.scrollHeight;
+        const winHeight = window.innerHeight;                    
+        if(window.scrollY > winHeight+doc/2-winHeight){
+            document.querySelector('.go-to-bottom').classList.add('toggle-go-to-bottom');  
+            document.querySelector('.go-to-bottom a').setAttribute("href", "#first-country")
+        }else{
+            document.querySelector('.go-to-bottom').classList.remove('toggle-go-to-bottom');            
+            document.querySelector('.go-to-bottom a').setAttribute("href", "#last-country")
+        }        
+
+    }   
     countries.forEach(function(country, index){               
         function toggelData(id){
             country.querySelectorAll('.info').forEach(info => {
