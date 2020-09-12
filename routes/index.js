1d54a97f9d9facbc1dabf3bb4b274dbadd2ccff5
Mohd Rizwan <iamrizwan40@gmail.com>
2020-09-12 04:36:56 -0700

    added manifest.json file

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,30 @@ const fetch = require('node-fetch');
 // Lat and Long
 const countryCords = []
 let allData;
-fetch('https://corona.lmao.ninja/v2/countries')
+let mapdataClone = []
+const url = 'https://corona.lmao.ninja/v2/countries';
+fetch('http://localhost:5000/js/countries.json')
     .then(res => res.json())
     .then(covidData => {
         covidData.forEach(data => {
             countryCords.push({code:data.countryInfo.iso2, lat:data.countryInfo.lat, long:data.countryInfo.long})
         })       
     })
-    .catch(err => console.log(err));    
-
-
-    fetch('https://corona.lmao.ninja/v2/countries')
+    .catch(err => console.log(err));        
+    fetch("http://localhost:5000/js/countries.json")
     .then(res => res.json())
     .then(data => {          
            allData = data
     })
-    .catch(err => console.log(err));         
+    .catch(err => console.log(err));       
+    
+
+
 Router.get('/', (req, res, next) => {    
     res.render('index', {
         title:"Covid19 Updates",
         allData,
-        countryCords
+        countryCords,                
     });
 })
 Router.get('/api/latlong', (req, res, next) => {
